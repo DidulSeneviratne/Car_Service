1672cc8697c3c2aafbc0cfd7d25e827e8d43e22a
Didul Seneviratne <didul.20211202@iit.ac.lk>
2025-12-28 20:57:38 +0530

    car_service initial

---
+++ carService/app/src/main/java/com/renuja/carService/OwnerHomeActivity.java
@@ -0,0 +1,14 @@
+package com.renuja.carService;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OwnerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_home);
+    }
+}
